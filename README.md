6aa366ad5f2ef816d0010d01d33bc401bf2fd5ed
michburkhalter <michburkhalter@users.noreply.github.com>
2024-02-19 13:15:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DaysToGo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/michburkhalter/DaysToGo)
---
+++ date_calculation.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+  init();
+  var t = setInterval(init, 1000);
+};
+
+function init() {
+  let target_date = new Date('2024-04-15 12:00');
+
+  document.getElementById('weeks_to_go').innerHTML =
+    getWeeksToDate(target_date);
+  document.getElementById('days_to_go').innerHTML = getDaysToDate(target_date);
+  document.getElementById('hours_to_go').innerHTML =
+    getHoursToDate(target_date);
+  document.getElementById('minutes_to_go').innerHTML =
+    getMinutesToDate(target_date);
+  document.getElementById('seconds_to_go').innerHTML =
+    getSecondsToDate(target_date);
+}
+
+function getWeeksToDate(target_date) {
+  delta = target_date - new Date();
+
+  delta = delta / 1000 / 60 / 60 / 24 / 7;
+
+  return delta.toFixed(1);
+}
+
+function getDaysToDate(target_date) {
+  delta = target_date - new Date();
+
+  delta = delta / 1000 / 60 / 60 / 24;
+
+  return delta.toFixed(0);
+}
+
+function getHoursToDate(target_date) {
+  delta = target_date - new Date();
+
+  delta = delta / 1000 / 60 / 60;
+
+  return delta.toFixed(0);
+}
+
+function getMinutesToDate(target_date) {
+  delta = target_date - new Date();
+
+  delta = delta / 1000 / 60;
+
+  return delta.toFixed(0);
+}
+
+function getSecondsToDate(target_date) {
+  delta = target_date - new Date();
+
+  delta = delta / 1000;
+
+  return delta.toFixed(0);
+}
