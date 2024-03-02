8fbede28bde768e24d04ce66cb72a20ccc85ed81
michburkhalter <michburkhalter@gmail.com>
2024-03-02 12:42:27 +0100

    d

--- public/sw.js
+++ public/sw.js
@@ -19,10 +19,25 @@ const APP_STATIC_RESOURCES = [
 
 ];
 
-self.addEventListener(’fetch’, function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request)
-        })
-    )
-})
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+self.addEventListener('fetch', (event) => {
+  // Check if this is a navigation request
+  if (event.request.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the network first
+      return fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request.url);
+      });
+    }));
+  } else {
+    return;
+  }
+});
